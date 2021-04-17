a0b4192470aeb3684aa5d348fc2db3690a190741
Harry <moomoofarm123123@gmail.com>
2021-04-17 13:45:35 +0100

    [TS-35] Update main.tf to include new vpc and rds modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,17 @@ provider "aws" {
     shared_credentials_file = "~/.aws/credentials"
 }
 
+module "vpc" {
+    source = "./VPC"
+}
+
 module "ec2" {
     source = "./EC2"
+    subnet_id = module.vpc.public_subnet_id
 }
+
+# module "rds" {
+#     source = "./RDS"
+#     database_password = var.database_password
+#     # subnet_id = module.vpc.private_subnet_id
+# }
