296d110d29d39209fbe29dff1bde58f93c1e9392
Harry <moomoofarm123123@gmail.com>
2021-04-18 20:55:17 +0100

    add bin bash to start of shell scripts, revert jenkinsfile2

--- kubernetes/open-application.sh
+++ kubernetes/open-application.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 chromium-browser $(kubectl get svc | grep -i nginx-reverse-proxy-loadbalancer | awk '{ print $4 }')
--- kubernetes/scripts/exec-kubectl.sh
+++ kubernetes/scripts/exec-kubectl.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 echo $1 $2 $3
 kubectl apply -f <(cat $3 | sed "s/db_hostname/$1/" | sed "s/db_password/$2/")
